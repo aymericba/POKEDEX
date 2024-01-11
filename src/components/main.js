c42bc92245e39ba56c0a2b7e21b0b4459116f53d
Lelia KOZAN <lelia.kozan@ecoles-epsi.net>
2024-01-11 10:28:44 +0100

    trier

--- src/components/main.js
+++ src/components/main.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { usePokemonContext } from '../PokemonContext';
 
 const Main = () => {
   const { pokemonList, types } = usePokemonContext();
+  const [sortedPokemonList, setSortedPokemonList] = useState([...pokemonList]);
+
+  const handleSort = (property) => {
+    let sortedList = [...sortedPokemonList];
+
+    switch (property) {
+      case 'number':
+        sortedList.sort((a, b) => a.id - b.id);
+        break;
+      case 'alphabetical':
+        sortedList.sort((a, b) => a.name.en.localeCompare(b.name.en));
+        break;
+      case 'weight':
+        sortedList.sort((a, b) => a.weight - b.weight);
+        break;
+      case 'height':
+        sortedList.sort((a, b) => a.height - b.height);
+        break;
+      default:
+        break;
+    }
+
+    setSortedPokemonList(sortedList);
+  };
 
   return (
     <div className="main">
       <h2>Liste des Pokémon</h2>
+      <div className="sort-buttons">
+        <button onClick={() => handleSort('number')}>Trier par Numéro</button>
+        <button onClick={() => handleSort('alphabetical')}>Trier par Ordre Alphabétique</button>
+        <button onClick={() => handleSort('weight')}>Trier par Poids</button>
+        <button onClick={() => handleSort('height')}>Trier par Taille</button>
+      </div>
       <ul className="pokemon-grid">
-        {pokemonList.map(pokemon => (
+        {sortedPokemonList.map((pokemon) => (
           <li key={pokemon.id}>
             <img src={pokemon.image} alt={pokemon.name.en} />
             <p>{`${pokemon.name.en} #${pokemon.id}`}</p>
             <p>Génération {pokemon.generation}</p>
             <div className="pokemon-types">
-              {pokemon.types.map(typeId => {
-                const type = types.find(t => t.id === typeId);
+              {pokemon.types.map((typeId) => {
+                const type = types.find((t) => t.id === typeId);
                 return (
                   <div key={type.id} className="pokemon-type">
                     <img src={type.image} alt={type.name.en} />
