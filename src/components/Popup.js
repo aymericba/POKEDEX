9bfea07054a883e5b93cb8582fadada77bdaf10e
aymericba <aymeric.barbot@neusta.fr>
2024-01-12 11:15:45 +0100

    SHINY

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,21 @@
 // Popup.js
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Popup = ({ pokemon, onClose }) => {
+  const [isShiny, setIsShiny] = useState(false);
+
+  const toggleShiny = () => {
+    setIsShiny((prevIsShiny) => !prevIsShiny);
+  };
+
   return (
     <div className="popup">
       <div className="popup-content">
-      <img src={pokemon.image} alt={pokemon.name.en} />
+        <header>
+          <button onClick={toggleShiny}>Shiny</button>
+        </header>
+        <img src={isShiny ? pokemon.image_shiny : pokemon.image} alt={pokemon.name.en} />
         <p>{`${pokemon.name.en} #${pokemon.id}`}</p>
         <p>Génération: {pokemon.generation}</p>
         {/* Ajoutez d'autres caractéristiques selon vos besoins */}
